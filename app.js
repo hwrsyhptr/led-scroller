c00288f3bc2e08b730524cf453cb86f2988e5d4c
hwrsyhptr <109587250+hwrsyhptr@users.noreply.github.com>
2022-07-19 16:15:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ window.requestAnimFrame = (function () {
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const customText = urlParams.get('text') || 'FOLLOW TIKTOK @dimasmiftah';
+const customText = urlParams.get('text') || 'aku minta maaf';
 const space = '                        ';
 const txt = space + customText;
 
